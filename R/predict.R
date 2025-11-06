2ec8d2c86e9b1f475b6b55b24d66f53cc9d2dc0d
koekvall <k.o.ekvall@gmail.com>
2025-11-06 15:12:39 -0500

    Revert "Update predict.R"
    This reverts commit 86be32bc6a980d740e258ea63a3b6486967bcb23.

--- R/predict.R
+++ R/predict.R
@@ -8,8 +8,22 @@
 #' @param num_nodes Number of nodes for Gaussian quadrature used to calculate predictions
 #' @return A vector of predicted (fitted) values
 #' @export
-pred_base <- function(X, Beta, sigma, link = "log", num_nodes = 15)
+pred_base <- function(X, Beta, sigma = NA, link_name = NA, num_nodes = 15, inv_link = NA)
 {
+
+  if(is.na(sigma)){
+    warning("sigma not supplied; setting to zeros")
+    sigma <- rep(0, n)
+  }
+  if(is.na(link_name) & is.na(inv_link)){
+    stop("Exactly one of link_name and inv_link is needed")
+  }
+
+  if(!is.na(link_name) & !is.na(inv_link)){
+    link <- NA
+    warning("link_name and inv_link both supplied; using inv_link and ignoring
+            link_name")
+  }
 
   # Define constants
   p <- ncol(X)
@@ -17,42 +31,31 @@ pred_base <- function(X, Beta, sigma, link = "log", num_nodes = 15)
   stopifnot(floor(n) == n, length(Beta) == p, length(sigma) == n)
   Xb <-as.vector(X %*% Beta)
 
-  if(link == "identity"){
-    return(Xb)
-  }
-
-  if(link == "log"){
-    Xb <- exp(Xb + 0.5 * sigma^2)
-    return(Xb)
-  }
-
-  if(link == "sqrt"){
-    Xb <- Xb^2 + sigma^2
-    return(Xb)
-  }
-
-  # Standard normal quadrature
-  grid_gauss <- mvQuad::createNIGrid(dim = 1, type = "GHN", level = num_nodes,
-                                     level.trans = FALSE)
-  nodes <- as.vector(mvQuad::getNodes(grid_gauss))
-  weights <- as.vector(mvQuad::getWeights(grid_gauss))
-  W0 <- matrix(rep(nodes, each = n), nrow = n, ncol = num_nodes,
-               byrow = FALSE)
-
-  if(link == "logit"){
-    W <- W0 * sigma
+  if(is.na(link_name)){
+    grid_gauss <- mvQuad::createNIGrid(dim = 1, type = "GHN", level = num_nodes,
+                                       level.trans = FALSE)
+    nodes <- as.vector(mvQuad::getNodes(grid_gauss))
+    weights <- as.vector(mvQuad::getWeights(grid_gauss))
+    W <- matrix(rep(nodes, each = n), nrow = n, ncol = num_nodes,
+                 byrow = FALSE)
+    W <- W * sigma
     W <- W + Xb
-
-    # Only the next line is specific to logit; could replace by any other link
-    W <- 1 / (1 + exp(-W))
-
+    W <- inv_link(W)
     W <- t(weights * t(W))
     Xb <- rowSums(W)
+  } else if(link_name == "identity"){
+    # Do nothing; Xb is prediction
+  } else if(link_name == "log"){
+    Xb <- exp(Xb + 0.5 * sigma^2)
+  } else if(link =="sqrt"){
+    Xb <- Xb^2 + sigma^2
   } else{
     warning("Requested link not implemented; returning NA")
     Xb <- rep(NA, n)
   }
-  return(Xb)
+
+  # Return
+  Xb
 }
 
 pred_glmer <- function(fit)
